db06045b60eed4660eadb45f3e79c3d423ce83d8
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-02-03 09:51:50 -0500

    Inc

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"log"
 	"os"
@@ -17,11 +19,30 @@ func main() {
 	ctx, cdpCancel := chromedp.NewContext(ctx)
 	defer cdpCancel()
 
-	if err := chromedp.Run(ctx, scrape()); err != nil {
+	var buf []byte
+	if err := chromedp.Run(ctx, scrape(&buf)); err != nil {
 		log.Fatalln(err)
+	}
+
+	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func scrape() chromedp.Tasks {
-	return chromedp.Tasks{}
+func scrape(res *[]byte) chromedp.Tasks {
+	var nodes []*cdp.Node
+	return chromedp.Tasks{
+		chromedp.Navigate(root_url),
+		chromedp.FullScreenshot(res, 100),
+		chromedp.Nodes("#a2zContainer > div.component-body-area > div.a2z-area", &nodes),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			var err error
+
+			for _, node := range nodes {
+				fmt.Println(node.Attributes)
+			}
+
+			return err
+		}),
+	}
 }
