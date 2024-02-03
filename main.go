250f33932abb4a1c5d7aa38c692553e63a6f1632
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-02-03 16:28:13 -0500

    More progress

--- main.go
+++ main.go
@@ -11,11 +11,16 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"time"
 )
 
 const root_url string = "https://www.questdiagnostics.com/healthcare-professionals/test-directory"
+
+type TestData struct {
+	name string
+}
 
 func main() {
 	ctx, notifyCancel := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -24,15 +29,25 @@ func main() {
 	ctx, cdpCancel := chromedp.NewContext(ctx)
 	defer cdpCancel()
 
-	if err := chromedp.Run(ctx, scrape()); err != nil {
+	var buf []byte
+	if err := chromedp.Run(ctx, scrape(&buf)); err != nil {
 		log.Fatalln(err)
+	}
+
+	if buf == nil || len(buf) == 0 {
+		return
+	}
+
+	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func scrape() chromedp.Tasks {
+func scrape(buf *[]byte) chromedp.Tasks {
 	var nodes []*cdp.Node
 	var specialtyUrls []string
-	var resultsMap = make(map[string]*cdp.Node)
+	var resultsMap = make(map[int]*cdp.Node)
+	var testData = make(map[int]TestData)
 
 	return chromedp.Tasks{
 		chromedp.Navigate(root_url),
@@ -40,9 +55,22 @@ func scrape() chromedp.Tasks {
 		chromedp.WaitReady("#a2zContainer"),
 		chromedp.Nodes("#a2zContainer > div.component-body-area > div.a2z-area > ul > li", &nodes),
 		chromedp.ActionFunc(crawlChildren(&nodes)),
-		//chromedp.ActionFunc(displayNodes(&nodes)),
 		chromedp.ActionFunc(gatherSpecialtyUrls(&nodes, &specialtyUrls)),
 		chromedp.ActionFunc(gatherTestPageUrls(&specialtyUrls, &resultsMap)),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			for id := range resultsMap {
+				if err := chromedp.Run(ctx,
+					chromedp.Navigate(fmt.Sprintf("https://testdirectory.questdiagnostics.com/test/test-detail/%d/complement-component-c3c?p=r&q=*&cc=MASTER", id)),
+					chromedp.Sleep(2*time.Second),
+					chromedp.FullScreenshot(buf, 100),
+					chromedp.ActionFunc(gatherTestData(id, &testData)),
+				); err != nil {
+					log.Printf("could not visit: %d", id)
+					log.Fatalln(err)
+				}
+			}
+			return nil
+		}),
 	}
 }
 
@@ -101,7 +129,7 @@ func printNodes(w io.Writer, nodes []*cdp.Node, padding, indent string) {
 	}
 }
 
-func gatherTestPageUrls(specialtyUrls *[]string, nodeMap *map[string]*cdp.Node) func(ctx context.Context) error {
+func gatherTestPageUrls(specialtyUrls *[]string, nodeMap *map[int]*cdp.Node) func(ctx context.Context) error {
 	return func(ctx context.Context) error {
 		var retErr error
 		var tmpNodes []*cdp.Node
@@ -114,11 +142,51 @@ func gatherTestPageUrls(specialtyUrls *[]string, nodeMap *map[string]*cdp.Node) 
 				chromedp.Sleep(10*time.Second),
 				chromedp.Nodes("md-card", &tmpNodes),
 			))
+
 			for _, tmpNode := range tmpNodes {
-				(*nodeMap)[tmpNode.AttributeValue("id")] = tmpNode
+				id := tmpNode.AttributeValue("id")
+				idNum, err := strconv.Atoi(strings.TrimPrefix(id, "MASTER"))
+				if err != nil {
+					continue
+				}
+				(*nodeMap)[idNum] = tmpNode
 			}
 			log.Printf("number of md-cards: %d", len(*nodeMap))
+			break
 		}
 		return retErr
 	}
+}
+
+func gatherTestData(id int, testDataMap *map[int]TestData) func(ctx context.Context) error {
+	return func(ctx context.Context) error {
+		var retErr error
+		testData := TestData{}
+		errors.Join(addName(ctx, &testData), retErr)
+		(*testDataMap)[id] = testData
+		return retErr
+	}
+}
+
+func addName(ctx context.Context, testData *TestData) error {
+	var nodes []*cdp.Node
+	chromedp.Run(ctx, chromedp.Nodes(".qd-header__title-mobile.ng-binding", &nodes))
+	if len(nodes) != 1 {
+		return errors.New("not one node returned for name search")
+	}
+	for _, node := range nodes {
+		if err := dom.RequestChildNodes(node.NodeID).WithDepth(-1).Do(ctx); err != nil {
+			return errors.New("could nto request child nodes")
+		}
+	}
+
+	node := nodes[0]
+	for _, child := range node.Children {
+		if child.NodeName == "#text" {
+			testData.name = child.NodeValue
+			break
+		}
+	}
+
+	return nil
 }
